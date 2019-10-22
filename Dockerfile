e06bc513626bd923dadd9406b9c558c331d303eb
Jon Nalley <(none)>
2019-10-22 16:35:54 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,51 @@
+FROM python:3.6.8-slim
+
+ARG USER=python
+ARG UID=999
+ARG PRECACHEPKGS="boto3 numpy pandas"
+
+RUN \
+  echo "deb http://deb.debian.org/debian stretch-backports main" \
+    > /etc/apt/sources.list.d/backports.list \
+  && LC_ALL=C apt-get update -yqq && LC_ALL=C apt-get dist-upgrade -yqq \
+  && LC_ALL=C apt-get install -yqq --no-install-recommends \
+    curl \
+    make \
+    unzip \
+    zip \
+  && LC_ALL=C apt-get -t stretch-backports install -yqq --no-install-recommends \
+    git \
+  && useradd \
+    --create-home \
+    --home-dir /app \
+    --skel /dev/null \
+    --shell /bin/false \
+    --uid ${UID} -U ${USER} \
+  && mkdir /var/cache/venv \
+  && chown ${UID}:${UID} /var/cache/venv \
+  && rm -rf \
+    /var/lib/apt/lists/* \
+    /tmp/* \
+    /var/tmp/* \
+    /usr/share/man \
+    /usr/share/doc \
+    /usr/share/doc-base
+
+USER ${USER}
+
+WORKDIR /app
+
+# entrypoint activates virtual environment
+COPY entrypoint.sh /usr/local/bin/entrypoint.sh
+
+RUN python -m venv /var/cache/venv && /usr/local/bin/entrypoint.sh \
+    pip install --upgrade \
+      pip \
+      awscli \
+      s3pypi \
+      setuptools \
+      git+https://git.corp.tc/python/tcpysetup@master \
+      git+https://git.corp.tc/python/tcpylib@master \
+      ${PRECACHEPKGS}
+
+ENTRYPOINT [ "/usr/local/bin/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+export PATH=/var/cache/venv/bin:${PATH}
+
+die() { echo "$*" && exit 1; }
+
+ispydatajob() {
+  python setup.py --keywords | grep -q tc-pydatajob && [[ -s Makefile ]]
+}
+
+pypackagename() {
+  python setup.py --name
+}
+
+pypackageversion() {
+  python setup.py --version
+}
+
+pypackage_upload() {
+  if [[ ${BUILDKITE_BRANCH:-unknown} != master ]]; then
+    echo "Skipping upload for non-master branch!"
+    return
+  fi
+  # --secret is just a prefix added to the object path
+  s3pypi --force --no-sdist --private --secret packages \
+    --bucket "${S3_BUCKET}" ||
+    die "Failed to publish package: $(pypackagename)"
+}
+
+pydatajob_upload() {
+  local branch="${BUILDKITE_BRANCH:-master}"
+  local name="$(pypackagename)"
+  local version="$(pypackageversion)"
+  local target="${name}-${version}"
+
+  [[ "${PREPEND_BRANCH_TO_FILENAME:-true}" == true && "${branch}" != "master" ]] &&
+    target="${name}-${branch}-${version}"
+
+  (make EXTRA_INDEX_URL="https://${EXTRA_INDEX_CREDENTIALS}@pypi.build.true.sh" &&
+    [[ -s "${name}.zip" ]]) || die "Failed to create ${name}.zip"
+  aws s3 cp "${name}.zip" \
+    "s3://${S3_BUCKET}/datajobs/${name}/${target}.zip" ||
+    die "Failed to copy ${name}.zip to s3://${S3_BUCKET}/datajobs/"
+}
+
+build() {
+  if ispydatajob; then
+    pydatajob_upload
+  else
+    pypackage_upload
+  fi
+}
+
+run() {
+  local DATA_JOB_URL="https://${EXTRA_INDEX_CREDENTIALS}@datajobs.build.true.sh"
+
+  package=$1 && shift
+  version=$1 && shift
+
+  [[ -n ${package} ]] || die "Missing pacakage name!"
+  [[ -n ${version} ]] || die "Missing version!"
+
+  curl -sSL -o package.zip \
+    "${DATA_JOB_URL}/${package}/${package}-${version}.zip" &&
+    unzip -qq package.zip &&
+    # show version
+    python3 -m "${package}" --version
+  # run
+  exec python3 -m "${package}" "$@"
+}
+
+"$@"
